ddd1fbc737cee83d8fb92eaa86ace7e81845522f
thanhluannn2008 <nguyenthanhluan.54183@gmail.com>
2023-11-20 11:13:00 +0700

    Trang chủ và 1 phần trang product

--- mvc/Bridge.php
+++ mvc/Bridge.php
@@ -7,4 +7,8 @@ require_once "./mvc/core/Controller.php";
 
 // Connect Database
 require_once "./mvc/core/DB.php";
+
+//How to call classes in a lib folder
+require_once "./mvc/lib/session.php";
+require_once "./mvc/lib/format.php";
 ?>
